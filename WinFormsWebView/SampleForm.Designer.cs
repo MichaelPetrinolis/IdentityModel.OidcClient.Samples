aa4b14862b6f7ca92428aca46544bb39bd8ed187
micha <micha@PYTHAGORAS>
2017-01-29 10:45:44 +0200

    use acr_values

--- WinFormsWebView/SampleForm.Designer.cs
+++ WinFormsWebView/SampleForm.Designer.cs
@@ -37,6 +37,10 @@
             this.IdentityTokenLabel = new System.Windows.Forms.Label();
             this.AccessTokenDisplay = new System.Windows.Forms.TextBox();
             this.CallApiButton = new System.Windows.Forms.Button();
+            this.TenantsListBox = new System.Windows.Forms.ListBox();
+            this.AuthorizeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.IdentityTokenDisplay = new System.Windows.Forms.TextBox();
             this.Inputs.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,11 +48,11 @@
             // 
             this.Inputs.ColumnCount = 5;
             this.Inputs.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.Inputs.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
-            this.Inputs.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.Inputs.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.Inputs.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
             this.Inputs.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.Inputs.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.Inputs.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.Inputs.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.Inputs.Controls.Add(this.LoginButton, 1, 0);
             this.Inputs.Controls.Add(this.LogoutButton, 2, 0);
             this.Inputs.Controls.Add(this.Silent, 4, 0);
@@ -57,24 +61,29 @@
             this.Inputs.Controls.Add(this.IdentityTokenLabel, 0, 2);
             this.Inputs.Controls.Add(this.AccessTokenDisplay, 1, 1);
             this.Inputs.Controls.Add(this.CallApiButton, 3, 0);
+            this.Inputs.Controls.Add(this.AuthorizeButton, 4, 4);
+            this.Inputs.Controls.Add(this.TenantsListBox, 1, 4);
+            this.Inputs.Controls.Add(this.label1, 0, 3);
+            this.Inputs.Controls.Add(this.IdentityTokenDisplay, 1, 3);
             this.Inputs.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Inputs.Location = new System.Drawing.Point(0, 0);
-            this.Inputs.Margin = new System.Windows.Forms.Padding(6);
             this.Inputs.Name = "Inputs";
-            this.Inputs.RowCount = 3;
+            this.Inputs.RowCount = 5;
             this.Inputs.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.Inputs.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.Inputs.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.Inputs.Size = new System.Drawing.Size(1248, 848);
+            this.Inputs.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.Inputs.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 270F));
+            this.Inputs.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Inputs.Size = new System.Drawing.Size(624, 441);
             this.Inputs.TabIndex = 0;
             // 
             // LoginButton
             // 
             this.LoginButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.LoginButton.Location = new System.Drawing.Point(172, 6);
-            this.LoginButton.Margin = new System.Windows.Forms.Padding(6);
+            this.LoginButton.Location = new System.Drawing.Point(88, 3);
             this.LoginButton.Name = "LoginButton";
-            this.LoginButton.Size = new System.Drawing.Size(188, 56);
+            this.LoginButton.Size = new System.Drawing.Size(94, 29);
             this.LoginButton.TabIndex = 0;
             this.LoginButton.Text = "Login";
             this.LoginButton.UseVisualStyleBackColor = true;
@@ -83,10 +92,9 @@
             // LogoutButton
             // 
             this.LogoutButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.LogoutButton.Location = new System.Drawing.Point(372, 6);
-            this.LogoutButton.Margin = new System.Windows.Forms.Padding(6);
+            this.LogoutButton.Location = new System.Drawing.Point(188, 3);
             this.LogoutButton.Name = "LogoutButton";
-            this.LogoutButton.Size = new System.Drawing.Size(188, 56);
+            this.LogoutButton.Size = new System.Drawing.Size(94, 29);
             this.LogoutButton.TabIndex = 1;
             this.LogoutButton.Text = "Logout";
             this.LogoutButton.UseVisualStyleBackColor = true;
@@ -96,10 +104,9 @@
             // 
             this.Silent.AutoSize = true;
             this.Silent.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Silent.Location = new System.Drawing.Point(1144, 6);
-            this.Silent.Margin = new System.Windows.Forms.Padding(6);
+            this.Silent.Location = new System.Drawing.Point(546, 3);
             this.Silent.Name = "Silent";
-            this.Silent.Size = new System.Drawing.Size(98, 56);
+            this.Silent.Size = new System.Drawing.Size(75, 29);
             this.Silent.TabIndex = 3;
             this.Silent.Text = "Silent";
             this.Silent.UseVisualStyleBackColor = true;
@@ -108,22 +115,20 @@
             // 
             this.Inputs.SetColumnSpan(this.OtherDataDisplay, 4);
             this.OtherDataDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.OtherDataDisplay.Location = new System.Drawing.Point(172, 117);
-            this.OtherDataDisplay.Margin = new System.Windows.Forms.Padding(6);
+            this.OtherDataDisplay.Location = new System.Drawing.Point(88, 64);
             this.OtherDataDisplay.Multiline = true;
             this.OtherDataDisplay.Name = "OtherDataDisplay";
             this.OtherDataDisplay.ReadOnly = true;
-            this.OtherDataDisplay.Size = new System.Drawing.Size(1070, 725);
+            this.OtherDataDisplay.Size = new System.Drawing.Size(533, 75);
             this.OtherDataDisplay.TabIndex = 7;
             // 
             // AccessTokenLabel
             // 
             this.AccessTokenLabel.AutoSize = true;
             this.AccessTokenLabel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.AccessTokenLabel.Location = new System.Drawing.Point(6, 68);
-            this.AccessTokenLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.AccessTokenLabel.Location = new System.Drawing.Point(3, 35);
             this.AccessTokenLabel.Name = "AccessTokenLabel";
-            this.AccessTokenLabel.Size = new System.Drawing.Size(154, 43);
+            this.AccessTokenLabel.Size = new System.Drawing.Size(79, 26);
             this.AccessTokenLabel.TabIndex = 0;
             this.AccessTokenLabel.Text = "Access Token:";
             this.AccessTokenLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -132,10 +137,9 @@
             // 
             this.IdentityTokenLabel.AutoSize = true;
             this.IdentityTokenLabel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.IdentityTokenLabel.Location = new System.Drawing.Point(6, 111);
-            this.IdentityTokenLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.IdentityTokenLabel.Location = new System.Drawing.Point(3, 61);
             this.IdentityTokenLabel.Name = "IdentityTokenLabel";
-            this.IdentityTokenLabel.Size = new System.Drawing.Size(154, 737);
+            this.IdentityTokenLabel.Size = new System.Drawing.Size(79, 81);
             this.IdentityTokenLabel.TabIndex = 0;
             this.IdentityTokenLabel.Text = "Other data:";
             this.IdentityTokenLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -144,30 +148,71 @@
             // 
             this.Inputs.SetColumnSpan(this.AccessTokenDisplay, 4);
             this.AccessTokenDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.AccessTokenDisplay.Location = new System.Drawing.Point(172, 74);
-            this.AccessTokenDisplay.Margin = new System.Windows.Forms.Padding(6);
+            this.AccessTokenDisplay.Location = new System.Drawing.Point(88, 38);
             this.AccessTokenDisplay.Name = "AccessTokenDisplay";
             this.AccessTokenDisplay.ReadOnly = true;
-            this.AccessTokenDisplay.Size = new System.Drawing.Size(1070, 31);
+            this.AccessTokenDisplay.Size = new System.Drawing.Size(533, 20);
             this.AccessTokenDisplay.TabIndex = 6;
             // 
             // CallApiButton
             // 
-            this.CallApiButton.Location = new System.Drawing.Point(569, 3);
+            this.CallApiButton.Location = new System.Drawing.Point(287, 2);
+            this.CallApiButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.CallApiButton.Name = "CallApiButton";
-            this.CallApiButton.Size = new System.Drawing.Size(152, 59);
+            this.CallApiButton.Size = new System.Drawing.Size(76, 31);
             this.CallApiButton.TabIndex = 8;
             this.CallApiButton.Text = "Call API";
             this.CallApiButton.UseVisualStyleBackColor = true;
             this.CallApiButton.Click += new System.EventHandler(this.CallApiButton_Click);
             // 
+            // TenantsListBox
+            // 
+            this.Inputs.SetColumnSpan(this.TenantsListBox, 3);
+            this.TenantsListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TenantsListBox.FormattingEnabled = true;
+            this.TenantsListBox.IntegralHeight = false;
+            this.TenantsListBox.Location = new System.Drawing.Point(88, 174);
+            this.TenantsListBox.Name = "TenantsListBox";
+            this.TenantsListBox.Size = new System.Drawing.Size(452, 264);
+            this.TenantsListBox.TabIndex = 9;
+            // 
+            // AuthorizeButton
+            // 
+            this.AuthorizeButton.Location = new System.Drawing.Point(546, 174);
+            this.AuthorizeButton.Name = "AuthorizeButton";
+            this.AuthorizeButton.Size = new System.Drawing.Size(75, 23);
+            this.AuthorizeButton.TabIndex = 10;
+            this.AuthorizeButton.Text = "Authorize";
+            this.AuthorizeButton.UseVisualStyleBackColor = true;
+            this.AuthorizeButton.Click += new System.EventHandler(this.AuthorizeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 29);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Access Token:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // IdentityTokenDisplay
+            // 
+            this.Inputs.SetColumnSpan(this.IdentityTokenDisplay, 4);
+            this.IdentityTokenDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.IdentityTokenDisplay.Location = new System.Drawing.Point(88, 145);
+            this.IdentityTokenDisplay.Name = "IdentityTokenDisplay";
+            this.IdentityTokenDisplay.ReadOnly = true;
+            this.IdentityTokenDisplay.Size = new System.Drawing.Size(533, 20);
+            this.IdentityTokenDisplay.TabIndex = 12;
+            // 
             // SampleForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1248, 848);
+            this.ClientSize = new System.Drawing.Size(624, 441);
             this.Controls.Add(this.Inputs);
-            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "SampleForm";
             this.Text = "SampleForm";
             this.Inputs.ResumeLayout(false);
@@ -186,5 +231,9 @@
         private System.Windows.Forms.Label IdentityTokenLabel;
         private System.Windows.Forms.TextBox AccessTokenDisplay;
         private System.Windows.Forms.Button CallApiButton;
+        private System.Windows.Forms.ListBox TenantsListBox;
+        private System.Windows.Forms.Button AuthorizeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox IdentityTokenDisplay;
     }
 }
--- WinFormsWebView/SampleForm.cs
+++ WinFormsWebView/SampleForm.cs
@@ -20,9 +20,9 @@ namespace WinForms
 
             var options = new OidcClientOptions
             {
-                Authority = "https://demo.identityserver.io",
+                Authority = "http://localhost:1941",
                 ClientId = "native.hybrid",
-                Scope = "openid email api offline_access",
+                Scope = "openid email philoxenon philoxenon.tenants offline_access",
                 RedirectUri = "http://localhost/winforms.client",
 
                 Browser = new WinFormsWebView()
@@ -44,9 +44,16 @@ namespace WinForms
         {
             AccessTokenDisplay.Clear();
             OtherDataDisplay.Clear();
-            
-            var result = await _oidcClient.LoginAsync(Silent.Checked);
+            IdentityTokenDisplay.Clear();
+            TenantsListBox.Items.Clear();
 
+            //var extraParameters = new { acr_values = $"idp:Facebook" };
+
+
+            var result = await _oidcClient.LoginAsync((Silent.Checked ?
+                IdentityModel.OidcClient.Browser.DisplayMode.Hidden :
+                IdentityModel.OidcClient.Browser.DisplayMode.Visible));
+                 
             if (result.IsError)
             {
                 MessageBox.Show(this, result.Error, "Login", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -54,11 +61,14 @@ namespace WinForms
             else
             {
                 AccessTokenDisplay.Text = result.AccessToken;
+                IdentityTokenDisplay.Text = result.IdentityToken;
 
                 var sb = new StringBuilder(128);
                 foreach (var claim in result.User.Claims)
                 {
                     sb.AppendLine($"{claim.Type}: {claim.Value}");
+                    if (claim.Type == "philoxenon_tenant")
+                        TenantsListBox.Items.Add(claim.Value);
                 }
 
                 if (!string.IsNullOrWhiteSpace(result.RefreshToken))
@@ -69,11 +79,11 @@ namespace WinForms
                 OtherDataDisplay.Text = sb.ToString();
 
                 _apiClient = new HttpClient(result.RefreshTokenHandler);
-                _apiClient.BaseAddress = new Uri("https://api.identityserver.io/");
+                _apiClient.BaseAddress = new Uri("http://localhost:5412/");
             }
         }
 
-        private async void LogoutButton_Click(object sender, EventArgs e)
+        private void LogoutButton_Click(object sender, EventArgs e)
         {
             //await _oidcClient.LogoutAsync(trySilent: Silent.Checked);
             //AccessTokenDisplay.Clear();
@@ -95,6 +105,72 @@ namespace WinForms
             else
             {
                 OtherDataDisplay.Text = result.ReasonPhrase;
+            }
+        }
+
+        private async void AuthorizeButton_Click(object sender, EventArgs e)
+        {
+            var selectedTenant = TenantsListBox.SelectedItem;
+            AccessTokenDisplay.Clear();
+            OtherDataDisplay.Clear();
+            IdentityTokenDisplay.Clear();
+            TenantsListBox.Items.Clear();
+
+            var options = new OidcClientOptions
+            {
+                Authority = "http://localhost:1941",
+                ClientId = "native.hybrid",
+                Scope = "openid email philoxenon offline_access",
+                RedirectUri = "http://localhost/winforms.client",
+                Browser = new WinFormsWebView()
+            };
+
+
+
+            //var options = new OidcClientOptions(
+            //    authority,
+            //    "openid email api offline_access",
+            //    "http://localhost/winforms.client",
+            //    "native", 
+            //    "secret", 
+            //    new WinFormsWebView());
+            //options.UseFormPost = true;
+
+            var oidcClient = new OidcClient(options);
+
+            //IdentityModel.OidcConstants.AuthorizeRequest.AcrValues
+
+            var extraParameters = new { acr_values = $"tenant:{selectedTenant}" };
+
+            var result = await oidcClient.LoginAsync(displayMode: Silent.Checked ? IdentityModel.OidcClient.Browser.DisplayMode.Hidden : IdentityModel.OidcClient.Browser.DisplayMode.Visible,
+                extraParameters: extraParameters);
+
+            if (result.IsError)
+            {
+                MessageBox.Show(this, result.Error, "Login", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                AccessTokenDisplay.Text = result.AccessToken;
+                IdentityTokenDisplay.Text = result.IdentityToken;
+
+                var sb = new StringBuilder(128);
+                foreach (var claim in result.User.Claims)
+                {
+                    sb.AppendLine($"{claim.Type}: {claim.Value}");
+                    if (claim.Type == "philoxenon_tenant")
+                        TenantsListBox.Items.Add(claim.Value);
+                }
+
+                if (!string.IsNullOrWhiteSpace(result.RefreshToken))
+                {
+                    sb.AppendLine($"refresh token: {result.RefreshToken}");
+                }
+
+                OtherDataDisplay.Text = sb.ToString();
+
+                _apiClient = new HttpClient(result.RefreshTokenHandler);
+                _apiClient.BaseAddress = new Uri("http://localhost:5412/");
             }
         }
     }
